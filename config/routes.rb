f0e937fe86c1d435d723bcc4a6eb74e55573b505
Sergio Marin <higher.vnf@gmail.com>
2015-09-03 22:10:22 -0300

    Add class 4 material

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,35 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root "contents#index"
 
-  # Example of regular route:
-  get 'products/:id' => 'products#view'
+  resources :groups
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  # verdo "path", "controlador#metodo"
+  # get "/index", "contents#index"
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # match "/index/:id", "contents#index"
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
+  # resources :products
+
+  # resources :products do
+  #   members do 
+  #     get :done, "tasks#mark_done"
   #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
+  #   collection do
+  #     get :alguna_accion, "tasks#alguna_accion"
   #   end
+  #   resources :tasks
+  # end
+    
+  # get 'products', "products#index"
+  # get 'products/:id', "products#show"
+  # post 'products', "products#show"
+  # get 'products/new', 'products#new'
+  # pacth 'products/:id', 'products#update'
+  # get 'products/:id/edit', 'products#edit'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  # put 'tasks/:id/done'
+  # put 'tasks/alguna_accion'
+  # put 'tasks/:id/undone'
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
